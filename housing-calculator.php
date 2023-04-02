58f3b09aff07e0d77a7f02900e6a58a1b7d659a7
Matthew Payne <matthewpco86@gmail.com>
2023-04-02 11:18:20 -0700

    add shortcode attributes

--- housing-calculator.php
+++ housing-calculator.php
@@ -3,7 +3,7 @@
 Plugin Name: Housing Calculator
 Plugin URI: https://github.com/Matthewpco/WP-Plugin-Housing-Calculator
 Description: A plugin that calculates the maximum amount you should spend on housing per month based on 30% of your gross monthly income.
-Version: 1.5.0
+Version: 1.6.0
 Author: Gary Matthew Payne
 Author URI: https://wpwebdevelopment.com/
 */
@@ -18,20 +18,13 @@ function housing_calculator_menu() {
 
 add_action('admin_menu', 'housing_calculator_menu');
 
-
-function housing_calculator_menu() {
-    add_menu_page('Housing Calculator', 'Housing Calculator', 'manage_options', 'housing-calculator', 'housing_calculator_page');
-}
-
-add_action('admin_menu', 'housing_calculator_menu');
-
 function housing_calculator_page() {
     housing_calculator_form();
 }
 
-function housing_calculator_form() {
+function housing_calculator_form($text_align = 'left') {
     ?>
-    <form method="post" style="padding: 2%;">
+    <form method="post" style="padding: 2%; text-align: <?php echo $text_align; ?>">
         <label for="income_type">Enter your income type:</label>
         <select name="income_type" id="income_type">
             <option value="hourly">Hourly</option>
@@ -64,17 +57,19 @@ function housing_calculator_form() {
 				$monthly_income = $income_amount;
 			}
 			$max_housing = $monthly_income * $income_percentage;
-			echo '<p style="padding: 2%;">Based on ' . ($_POST['income_percentage']) . '% of your monthly income, your maximum monthly housing expense should be: $' . number_format($max_housing, 2) . '</p>';
+			echo '<p style="text-align: '.$text_align.';">Based on your income and selected percentage, the maximum amount you should spend on housing per month is: $' . number_format($max_housing, 2) . '</p>';
 		} else {
-			echo '<p style="padding: 2%;">' . $_POST['income_amount'] . ' is not valid, please enter a valid number.</p>';
+			echo '<p style="color:red; text-align: '.$text_align.';">Please enter a valid income amount.</p>';
 		}
-		
-	}
+    }
 }
 
-function housing_calculator_shortcode() {
+function housing_calculator_shortcode($atts) {
     ob_start();
-    housing_calculator_form();
+    $atts = shortcode_atts(array(
+        'text_align' => 'left',
+    ), $atts);
+    housing_calculator_form($atts['text_align']);
     return ob_get_clean();
 }
 
